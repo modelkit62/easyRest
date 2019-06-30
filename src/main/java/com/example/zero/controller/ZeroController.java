c9a654f6f2b9a3539106755b36d16e5ea326f8e8
Daniel <modelkit62@gmail.com>
2019-06-30 14:17:02 +0100

    fourth commit

--- src/main/java/com/example/zero/controller/ZeroController.java
+++ src/main/java/com/example/zero/controller/ZeroController.java
@@ -23,12 +23,6 @@ public class ZeroController {
     @GetMapping
     public MyCurrency compare(@RequestBody MyCurrency myCurrency){
 
-        if(myCurrency.getCountry() == null){
-            myCurrency.setName("Los Cojones!");
-            myCurrency.setCountry("Los Cojones!");
-            myCurrency.setValue(25.3);
-        }
-
         return service.doCurrency(myCurrency);
     }
 
--- src/test/java/com/example/zero/controller/ZeroControllerTest_2.java
+++ src/test/java/com/example/zero/controller/ZeroControllerTest_2.java
@@ -1,10 +1,11 @@
 package com.example.zero.controller;
 
-/*import org.junit.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,9 +18,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 //@WebMvcTest(ZeroController.class)
 @SpringBootTest
-@AutoConfigureMockMvc*/
+@AutoConfigureMockMvc
 public class ZeroControllerTest_2 {
-/*
 
     @Autowired
     private MockMvc mockMvc;
@@ -27,13 +27,14 @@ public class ZeroControllerTest_2 {
     @Test
     public void testResultadoController() throws Exception {
         this.mockMvc.perform(
-                MockMvcRequestBuilders.get("/zero"))
-                // .content(json))
+                MockMvcRequestBuilders.get("/zero")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"Pound\",\"country\":\"UK\",\"value\":\"22.2\"}")
+                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
-                .andExpect(jsonPath("$.version").value("0.1"))
+                .andExpect(jsonPath("$.name").value("La ostia"))
                 .andDo(MockMvcResultHandlers.print());
     }
-*/
 
 }
