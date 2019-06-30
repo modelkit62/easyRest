c0a15f97d46c7182f67d068fa2d05c524d3c5949
Daniel <modelkit62@gmail.com>
2019-06-30 16:14:19 +0100

    sixth commit

--- src/test/java/com/example/zero/controller/ZeroControllerTest_2.java
+++ src/test/java/com/example/zero/controller/ZeroControllerTest_2.java
@@ -4,12 +4,15 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -32,19 +35,42 @@ public class ZeroControllerTest_2 {
     public void testResultadoController() throws Exception {
         this.mockMvc.perform(
                 MockMvcRequestBuilders.get("/zero")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(getIncommingRequest())
-                // .content("{\"name\":\"Pound\",\"country\":\"UK\",\"value\":\"22.2\"}")
-                .accept(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(getIncommingRequest())
+                        // .content("{\"name\":\"Pound\",\"country\":\"UK\",\"value\":\"22.2\"}")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/json;charset=UTF-8"))
                 .andExpect(jsonPath("$.name").value("La ostia"))
                 .andDo(MockMvcResultHandlers.print());
     }
 
+    @Test
+    public void testResultadoControllerWithAssertion() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.get("/zero")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(getIncommingRequest())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andReturn();
+
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+
+        JSONAssert.assertEquals(contentAsString, "{\"name\":\"La ostia\",\"country\":\"UK\",\"value\":25.5}", JSONCompareMode.LENIENT);
+
+    }
+
+
+    // with GUAVA can bring JSON from resources
     private String getIncommingRequest() throws IOException {
         return Resources.toString(Resources.getResource("testRequestJson/incoming-request.json"), Charsets.UTF_8);
 
     }
+
+    /*private String getExpectedResponseString(){ // EN PROSPECTSCONTROLLERIMPLTEST: getsGatewayIncomingRequest
+        Resource<MyCurrency> resource = new Resource<MyCurrency>()
+    }*/
+
 
 }
